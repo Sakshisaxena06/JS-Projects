4d1d49822c62f06c048c4f714ce5af47e9eb33b3
Sakshisaxena06 <saxenasakshi325@gmail.com>
2025-07-10 16:21:22 +0530

    NotesMaker project

---
+++ 4_Notes/script.js
@@ -0,0 +1,44 @@
+const notecontainer = document.querySelector(".notes-container") ;
+const createBtn = document.querySelector(".btn") ;
+let notes = document.querySelector(".input-box") ;
+
+function showNotes(){
+    notecontainer.innerHTML = localStorage.getItem("notes") ;
+}
+showNotes() ; 
+   
+// data store nhi ho rha h !
+function updatestorage(){
+    localStorage.setItem("notes" , notecontainer.innerHTML)
+}
+
+
+createBtn.addEventListener("click" ,() => {
+    let inputBox = document.createElement("p") ;
+    let img = document.createElement("img") ;
+    inputBox.className = "input-box" ;
+    inputBox.setAttribute("contenteditable" ,"true") ;
+    img.src = "Unknown-4.jpeg"
+    notecontainer.appendChild(inputBox).appendChild(img);
+})
+notecontainer.addEventListener("click" ,function(e){
+    if(e.target.tagName==="IMG"){
+        e.target.parentElement.remove() ;
+        updatestorage() ;
+
+    }
+    else if(e.target.tagName ==="P"){
+        notes = document.querySelector(".input-box")
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updatestorage() ;
+            }
+        })
+    }
+})
+document.addEventListener("keydown" ,event =>{
+    if(event.key=== "Enter"){
+        document.execCommand("insertLineBreak") ;
+        event.preventDefault() ;
+    }
+})
